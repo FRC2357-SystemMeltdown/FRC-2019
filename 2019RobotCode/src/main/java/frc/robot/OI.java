19daffcf969a7ada54e435833a8dcfa1b36503cf
Kevin Killingsworth <kevin@redfenix.com>
2019-02-05 21:30:08 -0600

    Add constants for controller ids

--- 2019RobotCode/src/main/java/frc/robot/OI.java
+++ 2019RobotCode/src/main/java/frc/robot/OI.java
@@ -4,29 +4,31 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
- * Add your docs here.
+ * Operator Interface
  */
 public class OI {
-    public XboxController driver = new XboxController(0);
-    public XboxController gunner = new XboxController(1);
+  public static final int CONTROLLER_ID_DRIVER = 0;
+  public static final int CONTROLLER_ID_GUNNER = 1;
 
-    public double getTurn(){
-        return driver.getX(Hand.kRight);
-    }
+  public XboxController driver = new XboxController(OI.CONTROLLER_ID_DRIVER);
+  public XboxController gunner = new XboxController(OI.CONTROLLER_ID_GUNNER);
 
-    public double getSpeed(){
-        return driver.getY(Hand.kLeft);
-    }
+  public double getTurn(){
+    return driver.getX(Hand.kRight);
+  }
 
-    public double getArmSpeed(){
-        double speed = gunner.getRawAxis(2) - gunner.getRawAxis(3);
-        return speed;
-    }
+  public double getSpeed(){
+    return driver.getY(Hand.kLeft);
+  }
+
+  public double getArmSpeed(){
+    double speed = gunner.getRawAxis(2) - gunner.getRawAxis(3);
+    return speed;
+  }
 }
