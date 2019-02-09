01029f31ed140348318572226375103788cb4457
Kevin Killingsworth <kevin@redfenix.com>
2019-02-09 10:22:03 -0600

    Overlays: Simplify to use constants directly

--- 2019RobotCode/src/main/java/frc/robot/overlays/DriverFailSafe.java
+++ 2019RobotCode/src/main/java/frc/robot/overlays/DriverFailSafe.java
@@ -8,21 +8,18 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
  * that relies on minimal to no sensors to function.
  */
 public class DriverFailSafe extends ControlOverlay implements ProportionalDrive {
-  public static final double DEFAULT_TURN_FACTOR = 1.0;
-  public static final double DEFAULT_SPEED_FACTOR = 1.0;
-
-  protected double turnFactor = DEFAULT_TURN_FACTOR;
-  protected double speedFactor = DEFAULT_SPEED_FACTOR;
+  public static final double TURN_FACTOR = 1.0;
+  public static final double SPEED_FACTOR = 1.0;
 
   public DriverFailSafe(XboxController controller) {
     super(controller);
   }
 
   public double getTurn() {
-      return controller.getX(Hand.kRight) * turnFactor;
+      return controller.getX(Hand.kRight) * TURN_FACTOR;
   }
 
   public double getSpeed() {
-      return controller.getY(Hand.kLeft) * speedFactor;
+      return controller.getY(Hand.kLeft) * SPEED_FACTOR;
   }
 }
--- 2019RobotCode/src/main/java/frc/robot/overlays/GunnerCreepDrive.java
+++ 2019RobotCode/src/main/java/frc/robot/overlays/GunnerCreepDrive.java
@@ -4,21 +4,18 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class GunnerCreepDrive extends ControlOverlay implements ProportionalDrive {
-  public static final double DEFAULT_TURN_FACTOR = 0.25;
-  public static final double DEFAULT_SPEED_FACTOR = 0.25;
-
-  protected double turnFactor = DEFAULT_TURN_FACTOR;
-  protected double speedFactor = DEFAULT_SPEED_FACTOR;
+  public static final double TURN_FACTOR = 0.25;
+  public static final double SPEED_FACTOR = 0.25;
 
   public GunnerCreepDrive(XboxController controller) {
     super(controller);
   }
 
   public double getTurn() {
-      return controller.getX(Hand.kRight) * turnFactor;
+      return controller.getX(Hand.kRight) * TURN_FACTOR;
   }
 
   public double getSpeed() {
-      return controller.getY(Hand.kLeft) * speedFactor;
+      return controller.getY(Hand.kLeft) * SPEED_FACTOR;
   }
 }
