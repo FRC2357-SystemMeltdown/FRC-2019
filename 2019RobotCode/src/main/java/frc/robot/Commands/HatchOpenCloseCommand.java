a27a8b5eaae4237b19d77df5c5b4a1741606869b
Alex <alex@tapdancinggoats.com>
2019-02-17 16:41:55 -0600

    Stop gantry when movement commands end
    This stops the hatch gantry when each movement command ends.

--- 2019RobotCode/src/main/java/frc/robot/Commands/HatchOpenCloseCommand.java
+++ 2019RobotCode/src/main/java/frc/robot/Commands/HatchOpenCloseCommand.java
@@ -48,6 +48,7 @@ public class HatchOpenCloseCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.HATCH_SUB.failsafeOpenCloseGantry(0);
   }
 
   // Called when another command which requires one or more of the same
--- 2019RobotCode/src/main/java/frc/robot/Commands/MoveHatchCommand.java
+++ 2019RobotCode/src/main/java/frc/robot/Commands/MoveHatchCommand.java
@@ -48,6 +48,7 @@ public class MoveHatchCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.HATCH_SUB.failsafeMoveGantry(0);
   }
 
   // Called when another command which requires one or more of the same
