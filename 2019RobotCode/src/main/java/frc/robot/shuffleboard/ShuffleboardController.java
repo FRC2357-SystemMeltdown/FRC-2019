7c3c9e1467b729a480bc00e91b85379fd6e598cf
Kevin Killingsworth <kevin@redfenix.com>
2019-02-28 16:41:12 -0600

    Add trim to proportional/failsafe drive (#35)

--- 2019RobotCode/src/main/java/frc/robot/shuffleboard/ShuffleboardController.java
+++ 2019RobotCode/src/main/java/frc/robot/shuffleboard/ShuffleboardController.java
@@ -1,10 +1,15 @@
 package frc.robot.shuffleboard;
 
 public class ShuffleboardController {
+  private SettingsTab settingsTab = new SettingsTab();
   private TestTab testTab = new TestTab();
 
   public void test() {
     testTab.show();
+  }
+
+  public void settings() {
+    settingsTab.show();
   }
 
   public void periodic() {
