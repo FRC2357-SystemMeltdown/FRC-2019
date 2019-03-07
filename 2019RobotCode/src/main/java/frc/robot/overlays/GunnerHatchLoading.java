d3d5333ef4760f5220167ecf818dc73cbda4b383
Kevin Killingsworth <kevin@redfenix.com>
2019-03-07 00:01:43 -0600

    Default hatch loading to open/close

--- 2019RobotCode/src/main/java/frc/robot/overlays/GunnerHatchLoading.java
+++ 2019RobotCode/src/main/java/frc/robot/overlays/GunnerHatchLoading.java
@@ -6,31 +6,25 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.RobotMap;
 import frc.robot.Commands.HatchDirectMoveCommand;
 import frc.robot.Commands.HatchDirectOpenCloseCommand;
-import frc.robot.Commands.HatchStopCommand;
 import frc.robot.Other.XboxRaw;
 
 public class GunnerHatchLoading extends GunnerLoading implements HatchControl {
 
   private HatchDirectOpenCloseCommand hatchOpenCloseCommand;
   private HatchDirectMoveCommand hatchMoveCommand;
-  private HatchStopCommand hatchStopCommand;
   private JoystickButton hatchMoveButton;
-  private JoystickButton hatchOpenCloseButton;
 
   public GunnerHatchLoading(XboxController controller) {
     super(controller, RobotMap.ARM_HATCH_LOW_GOAL_ANGLE);
 
     hatchOpenCloseCommand = new HatchDirectOpenCloseCommand(this);
     hatchMoveCommand = new HatchDirectMoveCommand(this);
-    hatchStopCommand = new HatchStopCommand();
 
     hatchMoveButton = new JoystickButton(controller, XboxRaw.X.value);
     hatchMoveButton.whenPressed(hatchMoveCommand);
-    hatchMoveButton.whenReleased(hatchStopCommand);
+    hatchMoveButton.whenReleased(hatchOpenCloseCommand);
 
-    hatchOpenCloseButton = new JoystickButton(controller, XboxRaw.Y.value);
-    hatchOpenCloseButton.whenPressed(hatchOpenCloseCommand);
-    hatchOpenCloseButton.whenReleased(hatchStopCommand);
+    hatchOpenCloseCommand.start();
   }
 
   @Override
