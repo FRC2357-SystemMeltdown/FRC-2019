045605a596e90048e2eb2748fcd3e73227b0040e
Kevin Killingsworth <kevin@redfenix.com>
2019-03-06 20:32:16 -0600

    GunnerCreepDrive - use proportional

--- 2019RobotCode/src/main/java/frc/robot/overlays/GunnerCreepDrive.java
+++ 2019RobotCode/src/main/java/frc/robot/overlays/GunnerCreepDrive.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import frc.robot.RobotMap;
 
-public class GunnerCreepDrive extends ControlOverlay implements VelocityDrive {
+public class GunnerCreepDrive extends ControlOverlay implements ProportionalDrive {
   public static final double TURN_FACTOR = RobotMap.GUNNER_TURN_PROPORTION;
   public static final double SPEED_FACTOR = RobotMap.GUNNER_SPEED_PROPORTION;
 
@@ -12,11 +12,13 @@ public class GunnerCreepDrive extends ControlOverlay implements VelocityDrive {
     super(controller);
   }
 
-  public double getTurnDegreesPerSecond() {
-      return controller.getX(Hand.kRight) * TURN_FACTOR * RobotMap.MAX_TURN_RATE_DEGREES_PER_SECOND;
+  @Override
+  public double getTurn() {
+      return controller.getX(Hand.kRight) * TURN_FACTOR;
   }
 
-  public double getSpeedInchesPerSecond() {
-      return controller.getY(Hand.kLeft) * SPEED_FACTOR * RobotMap.MAX_VELOCITY_INCHES_PER_SECOND;
+  @Override
+  public double getSpeed() {
+      return - (controller.getY(Hand.kLeft) * SPEED_FACTOR);
   }
 }
