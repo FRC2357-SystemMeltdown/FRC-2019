d4c7847fb477c6d9af983b46976d43ffd561272e
Kevin Killingsworth <kevin@redfenix.com>
2019-03-19 12:56:38 -0500

    Add button chords for auto modes
    This adds button chords for auto modes.
    
    Hold the back button to enter preview, ensure target is correct.
    Hold left bumper for hatch, hold right bumper for cargo.
    Press A for low goal, X for mid goal, Y for high goal, and B for loading

--- 2019RobotCode/src/main/java/frc/robot/controls/GunnerControls.java
+++ 2019RobotCode/src/main/java/frc/robot/controls/GunnerControls.java
@@ -10,12 +10,26 @@ public class GunnerControls extends Controls
   public static final double TURN_FACTOR = RobotMap.GUNNER_TURN_PROPORTION;
   public static final double SPEED_FACTOR = RobotMap.GUNNER_SPEED_PROPORTION;
 
-  public JoystickButton cargoRollerButton;
-  public JoystickButton armAdjustUpButton;
-  public JoystickButton armAdjustDownButton;
-  public JoystickButton autoModeButton;
-  public DPadTrigger armCycleUpTrigger;
-  public DPadTrigger armCycleDownTrigger;
+  public final JoystickButton cargoRollerButton;
+  public final JoystickButton armAdjustUpButton;
+  public final JoystickButton armAdjustDownButton;
+  public final JoystickButton autoModeButton;
+  public final JoystickButton autoHatchModeButton;
+  public final JoystickButton autoCargoModeButton;
+  public final JoystickButton autoScoreLowButton;
+  public final JoystickButton autoScoreMidButton;
+  public final JoystickButton autoScoreHighButton;
+  public final JoystickButton autoLoadingStationButton;
+  public final DPadTrigger armCycleUpTrigger;
+  public final DPadTrigger armCycleDownTrigger;
+  public final ButtonChordTrigger autoHatchLoadingStationTrigger;
+  public final ButtonChordTrigger autoHatchScoreLowTrigger;
+  public final ButtonChordTrigger autoHatchScoreMidTrigger;
+  public final ButtonChordTrigger autoHatchScoreHighTrigger;
+  public final ButtonChordTrigger autoCargoLoadingStationTrigger;
+  public final ButtonChordTrigger autoCargoScoreLowTrigger;
+  public final ButtonChordTrigger autoCargoScoreMidTrigger;
+  public final ButtonChordTrigger autoCargoScoreHighTrigger;
 
   public GunnerControls(XboxController controller) {
     super(controller);
@@ -24,8 +38,41 @@ public class GunnerControls extends Controls
     armAdjustUpButton = new JoystickButton(controller, XboxRaw.BumperRight.value);
     armAdjustDownButton = new JoystickButton(controller, XboxRaw.BumperLeft.value);
     autoModeButton = new JoystickButton(controller, XboxRaw.Back.value);
+    autoHatchModeButton = new JoystickButton(controller, XboxRaw.BumperLeft.value);
+    autoCargoModeButton = new JoystickButton(controller, XboxRaw.BumperRight.value);
+    autoScoreLowButton = new JoystickButton(controller, XboxRaw.A.value);
+    autoScoreMidButton = new JoystickButton(controller, XboxRaw.X.value);
+    autoScoreHighButton = new JoystickButton(controller, XboxRaw.Y.value);
+    autoLoadingStationButton = new JoystickButton(controller, XboxRaw.B.value);
+
     armCycleUpTrigger = new DPadTrigger(controller, DPadValue.Up);
     armCycleDownTrigger = new DPadTrigger(controller, DPadValue.Down);
+
+    autoHatchLoadingStationTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoHatchModeButton, autoLoadingStationButton}
+    );
+    autoHatchScoreLowTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoHatchModeButton, autoScoreLowButton}
+    );
+    autoHatchScoreMidTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoHatchModeButton, autoScoreMidButton}
+    );
+    autoHatchScoreHighTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoHatchModeButton, autoScoreHighButton}
+    );
+
+    autoCargoLoadingStationTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoCargoModeButton, autoLoadingStationButton}
+    );
+    autoCargoScoreLowTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoCargoModeButton, autoScoreLowButton}
+    );
+    autoCargoScoreMidTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoCargoModeButton, autoScoreMidButton}
+    );
+    autoCargoScoreHighTrigger = new ButtonChordTrigger(
+      new JoystickButton[] {autoModeButton, autoCargoModeButton, autoScoreHighButton}
+    );
   }
 
   @Override
