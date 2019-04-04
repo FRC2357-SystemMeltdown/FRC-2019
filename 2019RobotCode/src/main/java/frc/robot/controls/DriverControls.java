54fc9bd24a5a6e39136ee9bd56a89bc03162c0d6
Kevin Killingsworth <kevin@redfenix.com>
2019-04-04 16:52:28 -0500

    Driver: Use right trigger for slower movement.

--- 2019RobotCode/src/main/java/frc/robot/controls/DriverControls.java
+++ 2019RobotCode/src/main/java/frc/robot/controls/DriverControls.java
@@ -2,25 +2,33 @@ package frc.robot.controls;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.Other.Utility;
 
 public class DriverControls extends Controls implements ProportionalDrive, VelocityDrive {
+  public final DriverSlowTrigger slowTrigger;
+
   public DriverControls(XboxController controller) {
     super(controller);
+
+    slowTrigger = new DriverSlowTrigger(controller);
   }
 
   @Override
   public int getEncoderTurnDifferential() {
     double input = Utility.deadband(controller.getX(Hand.kRight), RobotMap.DRIVE_STICK_DEADBAND);
-    int turnRate = (int)(input * RobotMap.DRIVER_ENCODER_TURN_RATE);
+    int encoderTurn = Robot.OI.isDriverSlow() ? RobotMap.DRIVER_ENCODER_SLOW_TURN_RATE : RobotMap.DRIVER_ENCODER_TURN_RATE;
+    int turnRate = (int)(input * encoderTurn);
     return turnRate;
   }
 
   @Override
   public int getEncoderSpeed() {
+
     double input = Utility.deadband(controller.getY(Hand.kLeft), RobotMap.DRIVE_STICK_DEADBAND);
-    int speed = (int)(-input * RobotMap.DRIVER_ENCODER_SPEED);
+    int encoderSpeed = Robot.OI.isDriverSlow() ? RobotMap.DRIVER_ENCODER_SLOW_SPEED : RobotMap.DRIVER_ENCODER_SPEED;
+    int speed = (int)(-input * encoderSpeed);
     return speed;
   }
 
---
+++ 2019RobotCode/src/main/java/frc/robot/controls/DriverSlowTrigger.java
@@ -0,0 +1,21 @@
+package frc.robot.controls;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+import frc.robot.RobotMap;
+
+public class DriverSlowTrigger extends Trigger {
+  private XboxController controller;
+
+  public DriverSlowTrigger(XboxController controller) {
+    this.controller = controller;
+  }
+
+  @Override
+  public boolean get() {
+    double axis = controller.getTriggerAxis(Hand.kRight);
+
+    return (axis > RobotMap.DRIVER_SLOW_TRIGGER_THRESHOLD);
+  }
+}
